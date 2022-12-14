1b982ac4a470bd6ad006418c87088100fd23a1e9
Mehmet Yener YILMAZ <yilmazmehmetyener@gmail.com>
2022-12-15 00:08:29 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Kayit from './pages/kayit'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container">
+       <div className="jumbotron">
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-4" />
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p className="lead">
+          <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <Kayit />
     </div>
   );
 }
